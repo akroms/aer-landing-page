8d30b2188b0acc59fc48afdee2b31a763beb7718
akroms <Akromiphone7.90@gmail.com>
2022-02-02 09:31:22 +0500

    navbar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 /* Hover effect for search SVG icon */
-let searchIcon = window.document.querySelector(".search");
+const searchIcon = document.querySelector(".nav-search");
+const hamburgerMenu = document.querySelector(".nav-menu");
+const navItem = document.querySelectorAll(".nav-item");
+const navbar = document.querySelector(".nav");
 
 function hoverOver() {
     searchIcon.setAttribute("stroke", "#ffda69");
@@ -8,4 +11,30 @@ function hoverOver() {
 function leave() {
     searchIcon.setAttribute("stroke", "#ffffff");
 }
+
+/* Navigation menu Toggler */
+
+hamburgerMenu.addEventListener("click", () => {
+    if (hamburgerMenu.classList.contains("nav-menu")) {
+        hamburgerMenu.classList.replace("nav-menu", "hamburger");
+        searchIcon.style.display = "none";
+
+        for (let i = 0; i < navItem.length; i++) {
+            navItem[i].classList.add("item");
+        }
+        navbar.classList.add("navigation");
+        navbar.style.animationName = "slideOpen";
+    } else {
+        navbar.style.animationName = "slideClose";
+        setTimeout(() => {
+            hamburgerMenu.classList.replace("hamburger", "nav-menu");
+            navbar.classList.remove("navigation");
+            searchIcon.style.display = "block";
+            for (let i = 0; i < navItem.length; i++) {
+                navItem[i].classList.remove("item");
+            }
+        }, 1000);
+    }
+});
+
 
